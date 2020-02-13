ed8952753401a7092874661950628fccf56addfe
kofmanya <anna.belova7@gmail.com>
2020-02-14 00:39:58 +0100

    Part2 Week4

---
+++ FindPatternInText/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPatternInText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long p = 10000000007;
+            long x = 31;
+            long y = 1;
+
+            string pattern = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int pLength = pattern.Length;
+            int tLength = text.Length;
+
+            for(int i = 0; i < pLength; ++i)
+            {
+                y = (y * x) % p;
+                //y %= p;
+            }
+
+            long patternHash = PolyHash(pattern, p, x);
+
+            List<long> hashList = new List<long>();
+
+            for(int i = 0; i < tLength - pLength + 1; ++i)
+            {
+                hashList.Add(0);
+            }
+
+            hashList[tLength - pLength] = PolyHash(text.Substring(tLength - pLength), p, x);
+
+            for(int i = tLength - pLength - 1; i >= 0; --i)
+            {
+                hashList[i] = ((x * hashList[i + 1]) % p + (long)text[i] - (y * (long)text[i + pLength]) % p + p) % p;
+            }
+
+            for(int i = 0; i < hashList.Count(); ++i)
+            {
+                if (patternHash == hashList[i] && AreEqual(pattern, text.Substring(i, pLength)))
+                    Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+        public static long PolyHash(string str, long p, long x)
+        {
+            long ret = 0;
+
+            long xi = 1;
+            
+            for(int i = 0; i < str.Length; ++i)
+            {
+                ret += ((long)str[i] * xi) % p;
+
+                xi *= x;
+                xi %= p;
+            }
+
+            return ret % p;
+        }
+
+        public static bool AreEqual(string p, string s)
+        {
+            if (p.Length != s.Length)
+                return false;
+
+            for(int i = 0; i < p.Length; ++i)
+            {
+                if (p[i] != s[i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ SubstringEquality/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubstringEquality
+{
+    class Program
+    {
+        public static long p1 = 100000007;
+        public static long p2 = 100000009;
+        public static long x = 31;
+        public static List<long> powerX1;
+        public static List<long> powerX2;
+
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            FillPower(ref powerX1, str.Length, p1);
+            FillPower(ref powerX2, str.Length, p2);
+
+            List<long> hash1 = new List<long>();
+            List<long> hash2 = new List<long>();
+            
+            CalculateHash(str, ref hash1, p1);
+            CalculateHash(str, ref hash2, p2);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 0; i < n; ++i)
+            {
+                string[] s = Console.ReadLine().Split(new char[] { ' ' });
+
+                int a = Convert.ToInt32(s[0]);
+                int b = Convert.ToInt32(s[1]);
+                int l = Convert.ToInt32(s[2]);
+
+                long hash1A = CalculateSubstringHash(hash1, a, l, p1);
+                long hash1B = CalculateSubstringHash(hash1, b, l, p1);
+
+                long hash2A = CalculateSubstringHash(hash2, a, l, p2);
+                long hash2B = CalculateSubstringHash(hash2, b, l, p2);
+
+                if (hash1A == hash1B && hash2A == hash2B)
+                    Console.WriteLine("Yes");
+                else
+                    Console.WriteLine("No");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void FillPower(ref List<long> powerX, int n, long p)
+        {
+            powerX = new List<long>();
+            powerX.Add(1);
+
+            for (int i = 1; i <= n; ++i)
+            {
+                powerX.Add(powerX[i - 1] * x % p);
+            }
+        }
+
+        public static void CalculateHash(string s, ref List<long> res, long p)
+        {
+            res.Add(0);
+
+            for(int i = 1; i <= s.Length; ++i)
+            {
+                res.Add((res[i - 1] * x % p + (long)s[i - 1]) % p);
+            }
+        }
+
+        public static long CalculateSubstringHash(List<long> hash, int a, int l, long p)
+        {
+            return (hash[a + l] + p - (p == p1 ? powerX1[l] : powerX2[l]) * hash[a] % p) % p;
+        }
+    }
+}
