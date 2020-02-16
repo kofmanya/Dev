bf307e18595fb3d1d13b5109ed52547e2b79cc7f
kofmanya <anna.belova7@gmail.com>
2020-02-16 02:24:31 +0100

    Part2, Week4

---
+++ LongestCommonSubstring/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonSubstring
+{
+    class Program
+    {
+        public static long p1 = 100000007;
+        public static long p2 = 100000031;
+        
+        public static long x = 31;
+
+        public static List<long> powerX1 = new List<long>();
+        public static List<long> powerX2 = new List<long>();
+        
+        static void Main(string[] args)
+        {
+            FillPower(ref powerX1, 100000, p1);
+            FillPower(ref powerX2, 100000, p2);
+            
+            string[] str = Console.ReadLine().Split(new char[] { ' ' });
+            
+            while(str.Length > 1)
+            {
+                string s = str[0];
+                string t = str[1];
+
+                int n = s.Length;
+                int m = t.Length;
+
+                int left = 0;
+                int right = Math.Min(n, m) + 1;
+
+                int sPosition = 0;
+                int tPosition = 0;
+                int maxLength = 0;
+
+                while(right - left > 1)
+                {
+                    int middle = (left + right) / 2;
+
+                    List<long> currHashS1 = CalculateHashTableOptimized(powerX1, middle, p1, s);
+                    List<long> currHashS2 = CalculateHashTableOptimized(powerX2, middle, p2, s);
+                    
+                    List<long> currHashT1 = CalculateHashTableOptimized(powerX1, middle, p1, t);
+                    List<long> currHashT2 = CalculateHashTableOptimized(powerX2, middle, p2, t);
+                    
+                    bool hasCommonSubstring = false;
+
+                    Dictionary<long, int> dict = new Dictionary<long, int>();
+
+                    for(int i = 0; i < currHashS1.Count; ++i)
+                    {
+                        if(!dict.ContainsKey(currHashS1[i]))
+                            dict.Add(currHashS1[i], i);
+                    }
+
+                    for(int i = 0; i < currHashT1.Count; ++i)
+                    {
+                        if (dict.ContainsKey(currHashT1[i]) && currHashS2[dict[currHashT1[i]]] == currHashT2[i])
+                        {
+                            hasCommonSubstring = true;
+                            sPosition = dict[currHashT1[i]];
+                            tPosition = i;
+                            maxLength = middle;
+                            break;
+                        }
+                    }
+
+                    if (hasCommonSubstring)
+                        left = middle;
+                    else
+                        right = middle;
+                }
+
+                Console.WriteLine(sPosition + " " + tPosition + " " + maxLength);
+                str = Console.ReadLine().Split(new char[] { ' ' });
+            }
+        }
+
+        public static void FillPower(ref List<long> powerX, int l, long p)
+        {
+            powerX.Add(1);
+
+            for(int i = 1; i <= l; ++i)
+            {
+                powerX.Add(powerX[i - 1] * x % p);
+            }
+        }
+
+        public static List<long> CalculateHashTableOptimized(List<long> powerX, int l, long p, string str)
+        {
+            List<long> ret = new List<long>();
+
+            for(int i = 0; i <= str.Length - l; ++i)
+            {
+                ret.Add(0);
+            }
+
+            long polyHash = 0;
+
+            int power = 0;
+
+            for(int i = str.Length - l; i < str.Length; ++i)
+            {
+                polyHash += ((long)str[i] * powerX[power] % p);
+                polyHash %= p;
+                power++;
+            }
+            
+            ret[str.Length - l] = polyHash;
+
+            for(int i = str.Length - l - 1; i >= 0; --i)
+            {
+                ret[i] = (ret[i + 1] * x % p + (long)str[i] - powerX[l] * (long)str[i + l] % p + p) % p; 
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ PatternMatchingWithMismatches/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatchingWithMismatches
+{
+    class Program
+    {
+        public static long p = 1000000007;
+        public static long x = 31;
+        public static List<long> powerX = new List<long>();
+
+        static void Main(string[] args)
+        {
+            FillPower();
+
+            string[] str = Console.ReadLine().Split(new char[] { ' ' });
+            
+            while(str.Length > 1)
+            {
+                int k = Convert.ToInt32(str[0]);
+                
+                int n = str[1].Length;
+                int m = str[2].Length;
+
+                List<long> hashT = CalculateHash(str[1]);
+                List<long> hashP = CalculateHash(str[2]);
+
+                List<int> res = new List<int>();
+
+                for (int i = 0; i <= n - m; ++i)
+                {
+                    int indexT = i;
+                    int indexP = 0;
+
+                    int countMismatches = 0;
+
+                    while(indexP < m)
+                    {
+                        int currentLength = LargestCommonPrefix(hashT, hashP, indexT, indexP);
+
+                        if(currentLength == 0)
+                        {
+                            indexT++;
+                            indexP++;
+                            countMismatches++;
+                        }
+                        else
+                        {
+                            indexT += currentLength;
+                            indexP += currentLength;
+                        }
+
+                        if (countMismatches > k)
+                            break;
+                    }
+
+                    if (countMismatches <= k)
+                        res.Add(i);
+                     
+                }
+
+                Console.Write(res.Count() + " ");
+                for(int i = 0; i < res.Count(); ++i)
+                {
+                    Console.Write(res[i] + " ");
+                }
+                Console.WriteLine();
+
+                str = Console.ReadLine().Split(new char[] { ' ' });
+            }
+        }
+
+        public static void FillPower()
+        {
+            powerX.Add(1);
+
+            for(int i = 1; i <= 200000; ++i)
+            {
+                powerX.Add(powerX[i - 1] * x % p);
+            }
+        }
+
+        public static List<long> CalculateHash(string str)
+        {
+            List<long> ret = new List<long>();
+            ret.Add(0);
+
+            for(int i = 1; i <= str.Length; ++i)
+            {
+                ret.Add((ret[i - 1] * x % p + (long)str[i - 1]) % p);
+            }
+
+            return ret;
+        }
+
+        public static long CalculateSubstringHash(List<long> hash, int a, int l)
+        {
+            return (hash[a + l] + p - powerX[l] * hash[a] % p) % p;
+        }
+
+        public static int LargestCommonPrefix(List<long> hashT, List<long> hashP, int indexT, int indexP)
+        {
+            int left = 0;
+            int right = hashP.Count() - indexP;
+
+            while(right - left > 1)
+            {
+                int middle = (left + right) / 2;
+
+                long middleHashT = CalculateSubstringHash(hashT, indexT, middle);
+                long middleHashP = CalculateSubstringHash(hashP, indexP, middle);
+
+                if(middleHashT == middleHashP)
+                {
+                    left = middle;
+                }
+                else
+                {
+                    right = middle;
+                }
+            }
+
+            return left;
+        }
+    }
+}
